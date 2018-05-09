3bdb1c2decf0bf5cda473e2aef073c370dc43aa6
John Rogers <johnrogers@Johns-MacBook-Pro.local>
2018-05-09 11:35:41 -0500

    Clean client files.

--- server/model/db.js
+++ server/model/db.js
@@ -2,9 +2,9 @@ module.exports.knex = require("knex")({
   client: "pg",
   version: "7.2",
   connection: {
-    host: process.env.DB_HOST || "multiverseapidata.cbtesjedgl9t.us-east-2.rds.amazonaws.com",
-    user: process.env.DB_USER || "mrrogers",
-    password: process.env.DB_PW || "56234623asdn",
-    database: process.env.DB_DATABASE || "multiverse"
+    host: "multiverseapidata.cbtesjedgl9t.us-east-2.rds.amazonaws.com",
+    user: "mrrogers",
+    password: "56234623asdn",
+    database: "multiverse"
   }
 });
