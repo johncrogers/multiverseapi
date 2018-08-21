8e42486e5824399e2bf46528904bf46cbe4149d1
John Rogers <johnrogers@johns-mbp.attlocal.net>
2018-08-20 22:41:32 -0500

    Update config.

--- server/model/db.js
+++ server/model/db.js
@@ -2,9 +2,9 @@ module.exports.knex = require("knex")({
   client: "pg",
   version: "9.6",
   connection: {
-    host: "multiverseapidata.cbtesjedgl9t.us-east-2.rds.amazonaws.com",
-    user: "mrrogers",
-    password: "56234623asdn",
-    database: "multiverse"
+    host: process.env.awshost,
+    user: process.env.awsuser,
+    password: process.env.awspassword,
+    database: process.env.awsdatabase
   }
 });
